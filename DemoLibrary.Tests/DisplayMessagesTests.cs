0768321da9f66a9e72e8868a3340b1a3b08a237b
BriceDon <65740750+boodscode237@users.noreply.github.com>
2021-05-22 00:30:17 +0500

    Add project files.

---
+++ DemoLibrary.Tests/DisplayMessagesTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+//Arrange, Act, Assert
+// expected and actual
+namespace DemoLibrary.Tests
+{
+    public class DisplayMessagesTests
+    {
+        [Fact]
+        public void GreetingShouldReturnGoodMorningMessage()
+        {
+            // Arrange
+            DisplayMessagesClass messages = new DisplayMessagesClass();
+            string expected = "Good morning Donny";
+
+            // Act
+            string actual = messages.Greeting("Donny", 6);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GreetingShouldReturnGoodAfternoonMessage()
+        {
+            // Arrange
+            DisplayMessagesClass messages = new DisplayMessagesClass();
+            string expected = "Good afternoon Donny";
+
+            // Act
+            string actual = messages.Greeting("Donny", 16);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Joe", 0, "Good morning Joe")]
+        [InlineData("Joe", 1, "Good morning Joe")]
+        [InlineData("Joe", 2, "Good morning Joe")]
+        [InlineData("Joe", 4, "Good day Joe")]
+        [InlineData("Joe", 5, "Good morning Joe")]
+        [InlineData("Joe", 6, "Good morning Joe")]
+        [InlineData("Joe", 7, "Good morning Joe")]
+        public void GreetingShouldReturnExpectedValue(
+            string firstName,
+            int hourOfTheDay,
+            string expected)
+        {
+            // Arrange
+            DisplayMessagesClass messages = new DisplayMessagesClass();
+
+            // Act
+            string actual = messages.Greeting(firstName, hourOfTheDay);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DemoLibrary/DisplayMessagesClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class DisplayMessagesClass
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string output = "";
+            if (hourOfTheDay == 4)
+            {
+                output = $"Good day {firstName}";
+            }
+            else if (hourOfTheDay < 12)
+            {
+                output = $"Good morning {firstName}";
+            }else if (hourOfTheDay < 18)
+            {
+                output = $"Good afternoon {firstName}";
+            }
+            else
+            {
+                output = $"Good evening {firstName}";
+            }
+
+            return output;
+        }
+    }
+}
